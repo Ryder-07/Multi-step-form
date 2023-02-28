f327dbe77f1aad36f0c8efa06d90a87bd958e14e
Rudra Pratap Singh <65117190+Ryder-07@users.noreply.github.com>
2023-02-28 23:14:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+var page=document.querySelector("child")
+document.getElementById("btn1").addEventListener("click", () =>{
+    console.log("i love you");
+    page.style.marginLeft
+    =("100%");
+})
